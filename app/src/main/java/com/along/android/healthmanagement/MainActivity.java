0a918eb05c27f799a15114fc88382e046127981a
xiaoleiuta <xiaolei.ren2016@gmail.com>
2017-02-14 21:43:32 -0600

    xiaolei Feb 14, 21:43

--- app/src/main/java/com/along/android/healthmanagement/MainActivity.java
+++ app/src/main/java/com/along/android/healthmanagement/MainActivity.java
@@ -1,7 +1,10 @@
 package com.along.android.healthmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = (TextView)findViewById(R.id.tvUsername);
         tv.setText(username);
 
+        Button btn_profile;
+        btn_profile = (Button) findViewById(R.id.btn_profile);
+
+        btn_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
