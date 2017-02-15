15291673f984bbffa9ddf35ef33eceecd1998910
xiaoleiuta <xiaolei.ren2016@gmail.com>
2017-02-14 22:09:24 -0600

    xiaolei Feb 14, 21:43

---
+++ app/src/main/java/com/along/android/healthmanagement/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.along.android.healthmanagement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by renxiaolei on 2/14/16.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+    // display user informations
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
