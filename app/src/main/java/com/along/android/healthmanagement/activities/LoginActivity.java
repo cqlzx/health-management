eea6cb82f504db1341dd1276975da40d71e3f911
xiaoleiuta <xiaolei.ren2016@gmail.com>
2017-02-15 23:28:41 -0600

    Xiaolei Feb 15 23：28

--- app/src/main/java/com/along/android/healthmanagement/activities/LoginActivity.java
+++ app/src/main/java/com/along/android/healthmanagement/activities/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.along.android.healthmanagement.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,12 @@ public class LoginActivity extends BasicActivity {
 
                 if(user != null) {
                     if(user.getPassword().equals(password)) {
+
+                        SharedPreferences sp = getSharedPreferences("Login", Context.MODE_PRIVATE);
+                        sp.edit().putString("username",username).apply();
+                        sp.edit().putLong("uid",user.getId()).apply();
+
+
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         //intent.setClass(LoginActivity.this,MainActivity.class);
                         intent.putExtra("Username", username);
--- app/src/main/java/com/along/android/healthmanagement/activities/ProfileActivity.java
+++ app/src/main/java/com/along/android/healthmanagement/activities/ProfileActivity.java
@@ -1,30 +1,58 @@
 package com.along.android.healthmanagement.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.along.android.healthmanagement.R;
+import com.along.android.healthmanagement.entities.User;
+import com.along.android.healthmanagement.helpers.EntityManager;
 
 public class ProfileActivity extends AppCompatActivity {
     // display user informations
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        // actionBar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle("Profile");
+        // sharePreferences
+        SharedPreferences sp = getSharedPreferences("Login", Context.MODE_PRIVATE);
+        Long userIdS = sp.getLong("uid", 0);
+        String userNameS = sp.getString("username", null);
+
+        // name & email
+        TextView userNameText = (TextView) findViewById(R.id.username);
+        TextView emailText = (TextView) findViewById(R.id.email);
+        if (userNameS != null) {
+            userNameText.setText(userNameS);
+        }
+        // user
+        User user = EntityManager.findById(User.class, userIdS);
+        if(user != null){
+            emailText.setText(user.getEmail());
+            Log.d("--email--->>>>>>>", user.getEmail());
+        }
+
+
+
+//        googleSignIn = (Button) findViewById(R.id.btn_google_sign_in);
+//        forgetPassword = (Button) findViewById(R.id.btn_forget_password);
+
 
         // button
         Button btn_editprofile;
@@ -39,12 +67,6 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-//        DatabaseHelper helper = new DatabaseHelper(this);
-//
-//        HashMap<String, String> mapList = helper.searchUserInfo("xiaolei"); //
-//        TextView tvUsername=(TextView)findViewById(R.id.username);
-//        String testS=mapList.get("password");
-//        tvUsername.setText(testS);
     }
 
     @Override
