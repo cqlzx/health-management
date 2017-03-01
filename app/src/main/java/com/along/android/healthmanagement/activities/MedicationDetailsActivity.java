7a89ca10cde7d15f2aadb3271322eb547a32e89d
xiaoleiuta <xiaolei.ren2016@gmail.com>
2017-03-01 02:53:36 -0600

    xiaolei 3/1 2:50 1、change MedicationDetail display 2、MedicationDetailAdapter

--- app/src/main/java/com/along/android/healthmanagement/activities/MedicationDetailsActivity.java
+++ app/src/main/java/com/along/android/healthmanagement/activities/MedicationDetailsActivity.java
@@ -1,7 +1,5 @@
 package com.along.android.healthmanagement.activities;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -11,7 +9,7 @@ import android.widget.TextView;
 import com.along.android.healthmanagement.R;
 import com.along.android.healthmanagement.adapters.MedicationDetailAdapter;
 import com.along.android.healthmanagement.entities.Medicine;
-import com.along.android.healthmanagement.entities.User;
+import com.along.android.healthmanagement.entities.Prescription;
 import com.along.android.healthmanagement.helpers.EntityManager;
 
 import java.util.ArrayList;
@@ -26,54 +24,55 @@ public class MedicationDetailsActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        String selectedPrescriptionItemId = getIntent().getStringExtra("selectedPrescriptionItemId"); //selectedPrescriptionItemId
+        String selectedPrescriptionItemId = getIntent().getStringExtra("selectedPrescriptionItemId");
+        // got the prescription
+        Prescription prescription = EntityManager.findById(Prescription.class, Long.parseLong(selectedPrescriptionItemId));
 
-        TextView patientNameText = (TextView) findViewById(R.id.xlMDPatientName);
-        TextView doctorNameText = (TextView) findViewById(R.id.xlMDDoctorName);
-        TextView diseaseText = (TextView) findViewById(R.id.xlMDDisease);
-        TextView prescriptionDateText = (TextView) findViewById(R.id.xlMDPrescriptionDate);
+        TextView patientNameTV = (TextView) findViewById(R.id.xlMDPatientName);
+        TextView doctorNameTV = (TextView) findViewById(R.id.xlMDDoctorName);
+        TextView diseaseTV = (TextView) findViewById(R.id.xlMDDisease);
+        TextView prescriptionDateTV = (TextView) findViewById(R.id.xlMDPrescriptionDate);
 
-        // sharePreferences  UserId & Real Name
-        SharedPreferences sp = getSharedPreferences("Login", Context.MODE_PRIVATE);
-        Long userIdS = sp.getLong("uid", 0);
-        User user = EntityManager.findById(User.class, userIdS);
-        if(user != null) {
-            patientNameText.setText("Patient Name:"+ user.getRealname());
-        }
+        // Patient Name:
+        String patientText = "Patient name: " + (null != prescription.getPatientName() ? prescription.getPatientName() : "");
+        patientNameTV.setText(patientText);
+
+        // Doctor Name:
+        String doctorText = "Prescribed by: " + (null != prescription.getDoctorName() ? prescription.getDoctorName() : "");
+        doctorNameTV.setText(doctorText);
+
+        // Disease:
+        String diseaseText = "For " + (null != prescription.getDisease() ? prescription.getDisease() : "");
+        diseaseTV.setText(diseaseText);
+
+        // Prescription Date
+        String prescriptionDateText = "Prescription date: " + (null != prescription.getDisease() ? prescription.getStartDate() : "");
+        prescriptionDateTV.setText(prescriptionDateText);
+
         System.out.println("----->>>>>>>>>>>>");
+        System.out.println(prescription.getMedication());
+        String test = prescription.getMedication().substring(0, prescription.getMedication().indexOf(","));
+        System.out.println("----->>>>>>>>>>>>");
+        System.out.println(test);
 
-
-        doctorNameText.setText("Doctor Name: Bertram"); // prescription.getDoctorName()
-
-        diseaseText.setText("Disease: Have a cold"); // prescription.getDisease()
-
-        prescriptionDateText.setText("Prescription Date: 2/20,2017");
-        System.out.println(diseaseText.getText());
-
-        //listview  prescription.getMedication()
-
-        // Testing ----->
-        // Create a list of medicine
-        ArrayList<Medicine> medicines = new ArrayList<Medicine>();
-
-        // Get the actual data from the database
-        for(int i=1; i<=1;i++) {
-            Medicine medicine = new Medicine();
-            medicine.setName(i + ". Rabonik, Telsar, Telnol");
-//            medicine.setStartDate("StartDate:1/2,2017");
-//            medicine.setEndDate("EndDate:3/30,2017");
-
-            medicines.add(medicine);
-            medicine.save();
-        }
 
         List<Medicine> medicineList = Medicine.listAll(Medicine.class);
 
+        List<Medicine> currentMedicine = new ArrayList<Medicine>();
+        for(Medicine medicine : medicineList) {
+            //If today <= endDate, then add to current
+            try {
+                if (null !=  medicine.getId()) {
+                    currentMedicine.add(medicine);
+                }
+            } catch (NumberFormatException nfe) {
+                currentMedicine.add(medicine);
+            }
+        }
+
         MedicationDetailAdapter medicationDetailAdapter =
                 new MedicationDetailAdapter(this, medicineList);
-
         ListView listView = (ListView) findViewById(R.id.medication_detail_list);
-
         listView.setAdapter(medicationDetailAdapter);
 
     }
--- app/src/main/java/com/along/android/healthmanagement/adapters/MedicationDetailAdapter.java
+++ app/src/main/java/com/along/android/healthmanagement/adapters/MedicationDetailAdapter.java
@@ -1,24 +1,18 @@
 package com.along.android.healthmanagement.adapters;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.along.android.healthmanagement.R;
 import com.along.android.healthmanagement.entities.Medicine;
-import com.along.android.healthmanagement.entities.Prescription;
 
 import java.util.List;
 
-import static com.along.android.healthmanagement.R.id.xlMDDelete;
 
 /**
  * Created by renxiaolei on 2/26/16.
@@ -33,62 +27,36 @@ public class MedicationDetailAdapter extends ArrayAdapter<Medicine> {
 
     @NonNull
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         View listItemView = convertView;
 
         if(null == convertView) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_medication_detail, parent, false);
         }
 
-        final Medicine medicine = getItem(position);
+        Medicine medicine = getItem(position);
 
-        TextView medicines = (TextView) listItemView.findViewById(R.id.xlMDMedicationNames);
-        medicines.setText(medicine.getName());
-
-        TextView doctorName = (TextView) listItemView.findViewById(R.id.xlMDTimeTaken);
-//        doctorName.setText(medicine.getStartDate());
-
-        TextView disease = (TextView) listItemView.findViewById(R.id.xlMDTimeInterval);
-//        disease.setText(medicine.getEndDate());
-
-        LinearLayout llMDDelete = (LinearLayout) listItemView.findViewById(xlMDDelete);
-        llMDDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                showDeleteAlertDialog(medicine);
-                Log.d("--delete--->>>>>>>", medicine.getName());
-            }
-        });
+        //xlMDMedicineId
+        TextView medicineIdTV = (TextView) listItemView.findViewById(R.id.xlMDMedicineId);
+        medicineIdTV.setText(medicine.getId().toString());
+        //xlMDMedicineName
+        TextView medicineNameTV = (TextView) listItemView.findViewById(R.id.xlMDMedicineName);
+        String medicineNameText = (null != medicine.getTimings() ? medicine.getTimings() : "");
+        medicineNameTV.setText(medicineNameText);
+        //xlMDMedicineTimings
+        TextView medicineTimingsTV = (TextView) listItemView.findViewById(R.id.xlMDMedicineTimings);
+        String medicineTimingsText = "Timings: " + (null != medicine.getTimings() ? medicine.getTimings() : "");
+        medicineTimingsTV.setText(medicineTimingsText);
+        //xlMDMedicineFrequency
+        TextView medicineFrequencyTV = (TextView) listItemView.findViewById(R.id.xlMDMedicineFrequency);
+        String medicineFrequencyText = "Frequency: " + (null != medicine.getFrequency() ? medicine.getFrequency() : "");
+        medicineFrequencyTV.setText(medicineFrequencyText);
+        //xlMDMedicineQuantity
+        TextView medicineQuantityTV = (TextView) listItemView.findViewById(R.id.xlMDMedicineQuantity);
+        String medicineQuantityText = "Quantity: " + (null != medicine.getQuantity() ? medicine.getQuantity() : "");
+        medicineQuantityTV.setText(medicineQuantityText);
 
         return listItemView;
     }
 
-    private void showDeleteAlertDialog(final Prescription prescription) {
-        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
-        alert.setTitle("Confirm Delete");
-        alert.setMessage("Are you sure to delete this medication entry?");
-        alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                /* delete the medication entry from the database  */
-//                Medicine medicineRecord = Medicine.findById(Prescription.class, medicine.getId());
-//                medicineRecord.delete();
-//
-//                MedicationDetailAdapter.this.remove(medicine);
-//
-//                dialog.dismiss();
-            }
-        });
-        alert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                dialog.dismiss();
-            }
-        });
-
-        alert.show();
-    }
 }
