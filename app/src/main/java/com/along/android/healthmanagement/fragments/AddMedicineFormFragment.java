afe10c5bbb0087962d2c0486456227b2360548b4
Riten Vithlani <vithlani.riten@gmail.com>
2017-03-01 01:44:34 -0600

    Add time pickers and spinners

--- app/src/main/java/com/along/android/healthmanagement/fragments/AddMedicineFormFragment.java
+++ app/src/main/java/com/along/android/healthmanagement/fragments/AddMedicineFormFragment.java
@@ -3,12 +3,16 @@ package com.along.android.healthmanagement.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.along.android.healthmanagement.R;
 import com.along.android.healthmanagement.entities.Medicine;
@@ -34,17 +38,45 @@ public class AddMedicineFormFragment extends BasicFragment{
         Button btnCancelMedicine = (Button) view.findViewById(R.id.btnCancelMedicine);
 
         final EditText medicineName = (EditText) view.findViewById(R.id.etMedicineName);
-        final EditText medicineQty = (EditText) view.findViewById(R.id.etMedicineQty);
-        final EditText medicineTimings = (EditText) view.findViewById(R.id.etMedicineConsumptionTime);
-        final EditText medicineFrequency = (EditText) view.findViewById(R.id.etMedicineFrequency);
+        //final EditText medicineQty = (EditText) view.findViewById(R.id.etMedicineQty);
+
+        final Spinner spinner = (Spinner) view.findViewById(R.id.spMedicineQty);
+        spinner.getSelectedItem().toString();
+
+        final TextView medicineTimings = (TextView) view.findViewById(R.id.tvTimings);
+        final Spinner medicineFrequency = (Spinner) view.findViewById(R.id.spMedicineFrequency);
+
+        TextView tvAddTime = (TextView) view.findViewById(R.id.tvAddTime);
+        tvAddTime.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                Bundle data = new Bundle();
+                data.putString("whichTimer", "addMedicineTime");
+                newFragment.setArguments(data);
+                newFragment.show(getFragmentManager(), "addMedicineTime");
+            }
+        });
+
+        TextView tvResetTime = (TextView) view.findViewById(R.id.tvResetTime);
+        final LinearLayout llMedicineTimings = (LinearLayout) view.findViewById(R.id.llMedicineTimings);
+        tvResetTime.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                llMedicineTimings.setVisibility(View.GONE);
+                medicineTimings.setText("");
+            }
+        });
 
         btnSaveMedicine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Medicine medicine = new Medicine();
                 medicine.setName(medicineName.getText().toString());
-                medicine.setFrequency(medicineFrequency.getText().toString());
-                medicine.setQuantity(medicineQty.getText().toString());
+                medicine.setFrequency(medicineFrequency.getSelectedItem().toString());
+                medicine.setQuantity(spinner.getSelectedItem().toString());
                 medicine.setTimings(medicineTimings.getText().toString());
                 Long medicineId = medicine.save();
 
---
+++ app/src/main/java/com/along/android/healthmanagement/fragments/TimePickerFragment.java
@@ -0,0 +1,45 @@
+package com.along.android.healthmanagement.fragments;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import com.along.android.healthmanagement.R;
+
+import java.util.Calendar;
+
+/**
+ * Created by RitenVithlani on 2/21/17.
+ */
+
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        String displayTime = hourOfDay + ":" + minute;
+        if(null!= this.getArguments() && "addMedicineTime".equals(this.getArguments().getString("whichTimer"))) {
+            TextView txtView = (TextView) getActivity().findViewById(R.id.tvTimings);
+            txtView.setText(txtView.getText() + ", " + displayTime);
+
+            LinearLayout llMedicineTimings = (LinearLayout) getActivity().findViewById(R.id.llMedicineTimings);
+            llMedicineTimings.setVisibility(View.VISIBLE);
+        }
+    }
+}
